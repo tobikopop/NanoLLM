64be72e8950047383166b52ddafc4ab1c173d483
Beckett Hoefling <beckhoefling@me.com>
2025-05-28 11:03:24 -0400

    Revert "remove depreciated dependency"
    This reverts commit a98ffc40a3d1efb86000f55487c7f1c2ba4a9ace.

--- nano_llm/vision/clip.py
+++ nano_llm/vision/clip.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 # Deprecated:  this moved to github.com/dusty-nv/clip_trt
-# CLIP support disabled: clip_trt not installed
-CLIPVisionModel = None
-TIMMVisionModel = None
-
+from clip_trt import CLIPVisionModel, TIMMVisionModel
